1e599f0a9096307fcd7bf20275afb37dd3669f49
Interseptor06 <martin@chtilianov.com>
2023-01-31 16:36:24 +0200

    Testing use of local lib.rs in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use Cryptonic::{matrix::Matrix, layout::Layout};
+
 fn main() {
-    println!("Hello, Cryptonic!");
+    println!("Hello, World!");
+    let mut mat: Matrix<i32> = Matrix::new(vec![3, 4], Layout::RowMajor);
+    println!("{:?}", mat.shape);
 }
