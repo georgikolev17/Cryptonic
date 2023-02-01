86d43f768375acd987fbffb3a2ca4b0c7dcb29f7
Interseptor06 <martin@chtilianov.com>
2023-02-01 18:46:37 +0200

    CML-38 Test GitHub and Jira connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use Cryptonic::{matrix::Matrix, layout::Layout};
-
 fn main() {
-    println!("Hello, World!");
-    let mut mat: Matrix<i32> = Matrix::new(vec![3, 4], Layout::RowMajor);
-    println!("{:#?}", mat);
+    println!("GitHub and Jira are connected!");
 }
