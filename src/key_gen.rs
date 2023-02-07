28ec0a2e3b6a36238a9ed5e5262fea8a7288bf0b
Interseptor06 <martin@chtilianov.com>
2023-02-07 04:34:16 +0200

    Added key_gen.rs which will contain the code for the key generation for our custom type

---
+++ src/key_gen.rs
@@ -0,0 +1,5 @@
+// In this file I'll write the custom params for the mock examples in addition to which
+// I'll also implement ser/des functions whose results will be written directly to storage
+// and committed to allow faster build and test times.
+
+use concrete::parameters;
