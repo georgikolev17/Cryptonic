27efa924c70acf8654ddd8ad69b06c6ad0cb1321
Interseptor06 <martin@chtilianov.com>
2023-04-20 13:30:15 +0300

    Removed custom tensor and added ndarray support

--- src/neural_network/layer_trait.rs
+++ src/neural_network/layer_trait.rs
@@ -1,10 +1,14 @@
+use ndarray::prelude::*;
 
-pub trait Layer {
+
+// Dimension D Specifies the dimensionality of the Data including the layer also
+// The dimension D should look like (LayerSize, Data.Shape)
+pub trait Layer<D: Dimension> {
     type CType;
 
-    fn forward(&mut self, input : Matrix<Self::CType>) -> Matrix<Self::CType> where <Self as Layer>::CType: Clone + Default;
+    fn forward(&mut self, input : Array<Self::CType, D>) -> Array<Self::CType, D> where <Self as Layer<D>>::CType: Clone + Default;
 
-    fn get_input_shape(&self) -> &Vec<usize>;
+    fn get_input_shape(&self) -> &Array<usize, D>;
 
-    fn get_output_shape(&self) -> &Vec<usize>;
+    fn get_output_shape(&self) -> &Array<usize, D>;
 }
