9fcd0d988fe4c5fe5fe0a1203c7b1dc17ccd9e9e
Interseptor06 <martin@chtilianov.com>
2023-04-20 13:30:12 +0300

    Removed custom tensor and added ndarray support

--- src/neural_network/layer_type.rs
+++ src/neural_network/layer_type.rs
@@ -1,29 +1,31 @@
+use ndarray::prelude::*;
+
 use crate::neural_network::dense_layer::DenseLayer;
 use crate::neural_network::layer_trait::Layer;
-use crate::neural_network::test_layer::TestLayer;
+
 
 pub enum LayerType<T> {
     // When a layer is implemented, it will be added here
     DenseLayer(DenseLayer<T>),
 }
-impl<T> Layer for LayerType<T> {
+impl<T, D: Dimension> Layer<D> for LayerType<T> {
     type CType = T;
 
-    fn forward(&mut self, input : Matrix<T>)  -> Matrix<Self::CType> where <Self as Layer>::CType: Clone + Default {
+    fn forward(&mut self, input : Array<Self::CType, D>)  -> Array<Self::CType, D> where <Self as Layer<D>>::CType: Clone + Default {
         match self {
             // When a layer is implemented, it will be added here
             LayerType::DenseLayer(dense_layer) => return dense_layer.forward(input),
         }
     }
 
-    fn get_input_shape(&self) -> &Vec<usize> {
+    fn get_input_shape(&self) -> &Array<usize, D> {
         match self {
             // When a layer is implemented, it will be added here
             LayerType::DenseLayer(dense_layer) => return dense_layer.get_input_shape(),
         }
     }
 
-    fn get_output_shape(&self) -> &Vec<usize> {
+    fn get_output_shape(&self) -> &Array<usize, D> {
         match self {
             // When a layer is implemented, it will be added here
             LayerType::DenseLayer(dense_layer) => return dense_layer.get_output_shape(),
