c57811b614b6a60e8c1a98c373d7705162a9dcf9
Interseptor06 <martin@chtilianov.com>
2023-04-20 20:05:05 +0300

    Added std trait implementations and fixed CipherTextType struct

--- src/cryptography/ciphtxt.rs
+++ src/cryptography/ciphtxt.rs
@@ -1,14 +1,19 @@
+use tfhe::integer::ciphertext::{RadixCiphertext};
+use tfhe::integer::{gen_keys_radix, RadixClientKey, ServerKey, PublicKeyBig};
 use std::ops::{Add, AddAssign, Mul, MulAssign, Sub};
-use tfhe::shortint::prelude::*;
-
+use tfhe::shortint::ciphertext::KeyswitchBootstrap;
+use crate::cryptography::key_gen::custom_gen_keys;
 
 /// This struct is here to allow fast and easy flexibility and to limit the future problems
 /// of using concrete_integer::RadixCiphertext directly.
 ///
+#[derive(Clone)]
 pub struct CipherTextType{
-    pub CipherTxt: Option<Ciphertext>,
-    pub ServerKey: Option<ServerKey>,
-    pub Modulus: Option<u64>
+    pub CipherTxt: RadixCiphertext<KeyswitchBootstrap>,
+    pub PublicKey: PublicKeyBig,
+    pub ServerKey: ServerKey,
+    pub Zero: RadixCiphertext<KeyswitchBootstrap>,
+    pub IfDefRandClientKey: Option<RadixClientKey>
 }
 
 /// This impl block implements a basic constructor and a is_def() method. The is_def() method is
@@ -16,41 +21,35 @@ pub struct CipherTextType{
 /// Ciphertext or for ServerKey.
 ///
 impl CipherTextType{
-    fn new(_CipherTxt: Ciphertext, _ServerKey: ServerKey, _Modulus: u64) -> CipherTextType{
+    fn new(_CipherTxt: RadixCiphertext<KeyswitchBootstrap>, _PublicKey: PublicKeyBig, _ServerKey: ServerKey) -> CipherTextType{
         CipherTextType{
-            CipherTxt: Some(_CipherTxt),
-            ServerKey: Some(_ServerKey),
-            Modulus: Some(_Modulus)
+            CipherTxt: _CipherTxt,
+            PublicKey: _PublicKey.clone(),
+            ServerKey: _ServerKey,
+            Zero: _PublicKey.clone().encrypt_radix(0u64, 8),
+            IfDefRandClientKey: None
         }
     }
     fn is_def(&self) -> bool {
-        if self.CipherTxt.is_none() || self.ServerKey.is_none() || self.Modulus.is_none() {
+        if self.IfDefRandClientKey.is_none() {
             return true;
         }
         false
     }
 }
 
-impl Clone for CipherTextType{
-    fn clone(&self) -> Self {
-        if self.is_def() {
-            CipherTextType::new(self.CipherTxt.clone().unwrap(), self.ServerKey.clone().unwrap(), self.Modulus.clone().unwrap())
-        }
-        else{
-            // Error
-            CipherTextType::default()
-        }
-    }
-}
 
 /// Default trait implementation. CipherTxt and ServerKey are set to None which is then checked
 /// by the ops traits and return None if any of the two are None.
 impl Default for CipherTextType{
     fn default() -> Self {
+        let (ck, sk, pk) = custom_gen_keys();
         CipherTextType{
-            CipherTxt: None,
-            ServerKey: None,
-            Modulus: None
+            CipherTxt: pk.clone().encrypt_radix(0u64, 8),
+            PublicKey: pk.clone(),
+            ServerKey: sk,
+            Zero: pk.clone().encrypt_radix(0u64, 8),
+            IfDefRandClientKey: Some(ck)
         }
     }
 }
@@ -61,8 +60,8 @@ impl Add for CipherTextType {
 
     fn add(self, rhs: Self) -> Self::Output {
         if !self.is_def() && !rhs.is_def() {
-            let _ciphertext = self.ServerKey.clone().unwrap().unchecked_add(&self.CipherTxt.unwrap(), &rhs.CipherTxt.unwrap());
-            return Some(CipherTextType::new(_ciphertext, self.ServerKey.clone().unwrap(), self.Modulus.clone().unwrap()));
+            let _sum = self.ServerKey.unchecked_add(&self.CipherTxt, &rhs.CipherTxt);
+            return Some(CipherTextType::new(_sum, self.PublicKey, self.ServerKey));
         }
         else if !self.is_def() && rhs.is_def() {
             return Some(self.clone());
@@ -78,12 +77,11 @@ impl Add for CipherTextType {
 impl Mul for CipherTextType {
     type Output = Option<CipherTextType>;
 
-    fn mul(self, rhs: Self) -> Self::Output {
+    fn mul(mut self, mut rhs: Self) -> Self::Output {
         if !self.is_def() || !rhs.is_def() {
-            let _ciphertext = self.ServerKey.clone().unwrap().smart_mul_lsb(&mut self.CipherTxt.unwrap(), &mut rhs.CipherTxt.unwrap());
+            let _ciphertext = self.ServerKey.smart_mul(&mut self.CipherTxt, &mut rhs.CipherTxt);
 
-            Some(CipherTextType::new(_ciphertext, self.ServerKey.clone().unwrap(), self.Modulus.clone().unwrap()));
-
+            Some(CipherTextType::new(_ciphertext, self.PublicKey, self.ServerKey));
         }
         None
     }
@@ -95,9 +93,9 @@ impl Sub for CipherTextType {
 
     fn sub(self, rhs: Self) -> Self::Output {
         if !self.is_def() || !rhs.is_def() {
-            let _ciphertext = self.ServerKey.clone().unwrap().unchecked_sub(&self.CipherTxt.unwrap(), &rhs.CipherTxt.unwrap());
+            let _ciphertext = self.ServerKey.unchecked_sub(&self.CipherTxt, &rhs.CipherTxt);
 
-            Some(CipherTextType::new(_ciphertext, self.ServerKey.clone().unwrap(), self.Modulus.clone().unwrap()));
+            Some(CipherTextType::new(_ciphertext, self.PublicKey, self.ServerKey));
         }
         None
     }
@@ -108,8 +106,8 @@ impl Sub for CipherTextType {
 impl AddAssign for CipherTextType {
     fn add_assign(&mut self, rhs: Self) {
         if !self.is_def() || !rhs.is_def() {
-            let _ciphertext = self.ServerKey.clone().unwrap().unchecked_add(&self.CipherTxt.clone().unwrap(), &rhs.CipherTxt.clone().unwrap());
-            self.CipherTxt = Some(_ciphertext);
+            let _ciphertext = self.ServerKey.unchecked_add(&self.CipherTxt.clone(), &rhs.CipherTxt.clone());
+            self.CipherTxt = _ciphertext;
         }
     }
 }
@@ -118,8 +116,79 @@ impl AddAssign for CipherTextType {
 impl MulAssign for CipherTextType {
     fn mul_assign(&mut self, rhs: Self) {
         if !self.is_def() || !rhs.is_def() {
-            let _ciphertext = self.ServerKey.clone().unwrap().smart_mul_lsb(&mut self.CipherTxt.clone().unwrap(), &mut rhs.CipherTxt.clone().unwrap());
-            self.CipherTxt = Some(_ciphertext);
+            let _ciphertext = self.ServerKey.smart_mul(&mut self.CipherTxt.clone(), &mut rhs.CipherTxt.clone());
+            self.CipherTxt = _ciphertext;
+        }
+    }
+}
+
+
+
+
+/// Implements the Add trait
+impl Add<i32> for CipherTextType {
+    type Output = Option<CipherTextType>;
+
+    fn add(self, rhs: i32) -> Self::Output {
+        if !self.is_def() {
+            let _sum = self.ServerKey.unchecked_scalar_add(&self.CipherTxt, rhs as u64);
+            return Some(CipherTextType::new(_sum, self.PublicKey, self.ServerKey));
+        }
+        else if !self.is_def() {
+            return Some(self.clone());
+        }
+        else if self.is_def() {
+            return None;
+        }
+        None
+    }
+}
+
+/// Implements the Mul trait
+impl Mul<i32> for CipherTextType {
+    type Output = Option<CipherTextType>;
+
+    fn mul(mut self, mut rhs: i32) -> Self::Output {
+        if !self.is_def() {
+            let _ciphertext = self.ServerKey.smart_mul(&mut self.CipherTxt, &mut self.PublicKey.encrypt_radix(rhs as u64, 8));
+
+            Some(CipherTextType::new(_ciphertext, self.PublicKey, self.ServerKey));
+        }
+        None
+    }
+}
+
+/// Implements the Sub trait
+impl Sub<i32> for CipherTextType {
+    type Output = Option<CipherTextType>;
+
+    fn sub(self, rhs: i32) -> Self::Output {
+        if !self.is_def() {
+            let _ciphertext = self.ServerKey.unchecked_scalar_sub(&self.CipherTxt, rhs as u64);
+
+            Some(CipherTextType::new(_ciphertext, self.PublicKey, self.ServerKey));
+        }
+        None
+    }
+}
+
+
+/// Implements the AddAssign trait
+impl AddAssign<i32> for CipherTextType {
+    fn add_assign(&mut self, rhs: i32) {
+        if !self.is_def() {
+            let _ciphertext = self.ServerKey.unchecked_scalar_add(&self.CipherTxt.clone(), rhs as u64);
+            self.CipherTxt = _ciphertext;
+        }
+    }
+}
+
+/// Implements the MulAssign trait
+impl MulAssign<i32> for CipherTextType {
+    fn mul_assign(&mut self, rhs: i32) {
+        if !self.is_def() {
+            let _ciphertext = self.ServerKey.smart_mul(&mut self.CipherTxt.clone(), &mut self.PublicKey.encrypt_radix(rhs as u64, 8));
+            self.CipherTxt = _ciphertext;
         }
     }
 }
