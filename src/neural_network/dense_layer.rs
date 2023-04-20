fc135e302354383908e1725941c9665d4f2edb42
Interseptor06 <martin@chtilianov.com>
2023-04-20 13:30:06 +0300

    Removed custom tensor and added ndarray support

--- src/neural_network/dense_layer.rs
+++ src/neural_network/dense_layer.rs
@@ -1,30 +1,38 @@
 use std::marker::PhantomData;
 use crate::neural_network::layer_trait::Layer;
+use ndarray::prelude::*;
+
+/*
+    In the case of the dense layer input shape will be equal to the size of the layer
+    which is why the input shape is one-dimensional = Ix1
+
+    This could l
+*/
 
 pub struct DenseLayer<T> {
-    input_shape : Vec<usize>,
-    output_shape : Vec<usize>,
+    input_shape : Array<usize, Ix1>,
+    output_shape : Array<usize, Ix1>,
     _phantom : PhantomData<T>
 }
 
-impl<T> Layer for DenseLayer<T> {
+impl<T, D: Dimension> Layer<D> for DenseLayer<T> {
     type CType = T;
 
-    fn forward(&mut self, input: Matrix<Self::CType>) -> Matrix<Self::CType> where <Self as Layer>::CType: Clone + Default {
+    fn forward(&mut self, input: Array<Self::CType, D>) -> Array<Self::CType, D> where <Self as Layer<D>>::CType: Clone + Default {
         input
     }
 
-    fn get_input_shape(&self) -> &Vec<usize> {
+    fn get_input_shape(&self) -> &Array<usize, D> {
         &self.input_shape
     }
 
-    fn get_output_shape(&self) -> &Vec<usize> {
+    fn get_output_shape(&self) -> &Array<usize, D> {
         &self.output_shape
     }
 }
 
-impl<T> DenseLayer<T> {
-    pub fn new(input_shape : Option<Vec<usize>>, output_shape : Option<Vec<usize>>) -> DenseLayer<T> {
+impl<T, D: Dimension> DenseLayer<T> {
+    pub fn new(input_shape : Option<Array<usize, D>>, output_shape : Option<Array<usize, D>>) -> DenseLayer<T> {
         DenseLayer {
             input_shape : match input_shape {
                 Some(shape) => shape,
