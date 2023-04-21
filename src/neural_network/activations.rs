4eff1e6766cf461b18230cf9f91008a9744daefd
Georgi Kolev <g.t.kolev1@gmail.com>
2023-04-21 23:24:14 +0300

    Removed unnecessary activation file

--- src/neural_network/activations.rs
+++
@@ -1,7 +0,0 @@
-// We assume T::default() is the 0 of the type T
-pub fn relu_usize<T>(_in: T) -> T where T: PartialOrd + Default{
-    if _in < T::default() {
-        return T::default();
-    }
-    _in
-}
