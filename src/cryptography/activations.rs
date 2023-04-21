175bafe3bbdfa48b26de97ff43fcf0d7f573926d
Interseptor06 <martin@chtilianov.com>
2023-04-21 23:28:45 +0300

    Changed activations.rs

--- src/cryptography/activations.rs
+++ src/cryptography/activations.rs
@@ -1,6 +1,7 @@
-use tfhe::integer::ciphertext::RadixCiphertext;
-use tfhe::shortint::ciphertext::KeyswitchBootstrap;
 use crate::cryptography::ciphtxt::CipherTextType;
+use tfhe::integer::ciphertext::RadixCiphertext;
+use tfhe::integer::RadixClientKey;
+use tfhe::shortint::ciphertext::KeyswitchBootstrap;
 
 // Considering we're working with 16-bit unsigned integers our range
 // will be from 0-65536, with a midpoint at 32,768 which we'll consider
@@ -16,11 +17,12 @@ pub fn binary_step(mut item: CipherTextType) -> RadixCiphertext<KeyswitchBootstr
     one
 }
 
-pub fn relu(mut item: CipherTextType) -> RadixCiphertext<KeyswitchBootstrap>{
-    let mut zero = item.PublicKey.encrypt_radix(32768u64, 8);
-
-    if item.ServerKey.smart_lt(&mut item, &mut zero) {
-        return zero;
+pub fn relu(mut elem:CipherTextType, rck: RadixClientKey) -> RadixCiphertext<KeyswitchBootstrap>{
+    let mut zero = elem.PublicKey.encrypt_radix(32768u64, 8);
+    let res = elem.ServerKey.smart_lt(&mut elem.CipherTxt, &mut zero);
+    let bool_res = rck.decrypt::<u64, KeyswitchBootstrap>(&res);
+    if bool_res == 1u64 {
+        return elem.PublicKey.encrypt_radix(32768u64, 8);
     }
-    item.CipherTxt
+    return elem.PublicKey.encrypt_radix(65536u64, 8);
 }
